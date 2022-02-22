a1d987719d91b20dcf45a976bac22578e4e1c149
liangwang <liangwang007@gmail.com>
2022-02-22 13:38:20 +0800

    mvc使用示例及注意事项

--- include/smart_thread_pool.h
+++ include/smart_thread_pool.h
@@ -781,8 +781,122 @@ namespace stp {
   };
 
 }   // namespace stp
+#endif  // SMART_THREAD_POOL_H_
+
+/*
+#include "smart_thread_pool.h"
+using stp::SmartThreadPoolBuilder;
+using stp::TaskPriority;
+#define SmartThreadUIFlag "SmartThreadOfUI"
+#define SmartThreadWorkerFlag "SmartThreadOfWorker"
+
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+
+std::shared_ptr<stp::SmartThreadPool> smartThreadPool; 
+
+struct Viewer: public std::enable_shared_from_this<Viewer> {
+  Viewer( std::string f){
+    flag = f;
+  }
+  std::string flag;
+  void onUserInputToSum(int from, int to);
+  void updateUI(std::string str) {
+    std::cout << str << std::endl;
+  }
+};
+
+struct Controler: public std::enable_shared_from_this<Controler> {
+  Controler( std::string f){
+    flag = f;
+  }
+  std::string flag;
+  void onSum(int from, int to);
+};
+
+struct Module: public std::enable_shared_from_this<Module> {
+  Module( std::string f){
+    flag = f;
+  }
+  std::string flag;
+  void doSum(int from, int to);
+};
+
+std::shared_ptr<Viewer> getViewer(){
+  static std::shared_ptr<Viewer> mgr(new Viewer(""));
+  return mgr;
+}
+std::shared_ptr<Controler> getControler(){
+  static std::shared_ptr<Controler> mgr(new Controler(""));
+  return mgr;
+}
+std::shared_ptr<Module> getModule(){
+  static std::shared_ptr<Module> mgr(new Module(""));
+  return mgr;
+}
+
+void Viewer::onUserInputToSum(int from, int to) {
+  smartThreadPool->ApplyAsync(SmartThreadUIFlag, TaskPriority::MEDIUM, std::bind(&Controler::onSum, getControler(), from, to));
+}
+void Controler::onSum(int from, int to){
+  smartThreadPool->ApplyAsync(SmartThreadWorkerFlag, TaskPriority::MEDIUM, std::bind(&Module::doSum, getModule(), from, to));
+  std::stringstream out;
+  out << "sum from:" << from << " to:" << to << " 计算中，请稍等";
+  smartThreadPool->ApplyAsync(SmartThreadUIFlag, TaskPriority::MEDIUM, std::bind(&Viewer::updateUI, getViewer(), out.str()));
+}
+void Module::doSum(int from, int to){
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+  int sum = 0;
+  for(int i = from; i <to; i++) {
+    sum += i;
+  } 
+  std::stringstream out;
+  out << "sum from:" << from << " to:" << to << " is: " << sum;
+  smartThreadPool->ApplyAsync(SmartThreadUIFlag, TaskPriority::MEDIUM, std::bind(&Viewer::updateUI, getViewer(), out.str()));
+}
+
+void testMVC() {
+  int from = std::rand() % 100;
+  int to = std::rand() % 100;
+  if (from > to) {
+    int tmp = to; to = from; from = tmp;
+  }
+  smartThreadPool->ApplyAsync(SmartThreadUIFlag, TaskPriority::MEDIUM, std::bind(&Viewer::onUserInputToSum, getViewer(), from, to));
+  smartThreadPool->ApplyDelayAsync(SmartThreadWorkerFlag, TaskPriority::MEDIUM,  std::chrono::seconds(6), testMVC);
+}
+
+int main(int argc, char** argv) {
+  SmartThreadPoolBuilder builder;
+  builder.AddClassifyPool(SmartThreadUIFlag, 1, 1)
+    .AddClassifyPool(SmartThreadWorkerFlag, 1, 1);
+  smartThreadPool = builder.BuildAndInit();
+
+  //mvc使用线程池模拟，尽量不要等待函数的执行结果，而是有结果时异步通知。
+  smartThreadPool->ApplyDelayAsync(SmartThreadWorkerFlag, TaskPriority::MEDIUM,  std::chrono::seconds(1), testMVC);
+
+  smartThreadPool->StartAllWorkers();
+  //任务池没有任务时不会主动退出，ctrl + c 退出
+}
+*/
+
 
 /* example:
+#include "smart_thread_pool.h"
+using stp::SmartThreadPoolBuilder;
+using stp::TaskPriority;
+
+#define SmartThreadUIFlag "SmartThreadOfUI"
+#define SmartThreadWorkerFlag "SmartThreadOfWorker"
+#define SmartThreadCPUFlag "SmartThreadOfCPU"
+#define SmartThreadIOFlag "SmartThreadOfIO"
+
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+
+std::shared_ptr<stp::SmartThreadPool> smartThreadPool; 
+
 
 double my_divide(double x, double y) { 
   printf("my_divide\n");
@@ -802,7 +916,7 @@ struct MyPair {
     return c/d; }
 };
 
-struct SharedMyPair {
+struct SharedMyPair: public std::enable_shared_from_this<SharedMyPair> {
   ~SharedMyPair(){
     printf("SharedMyPair destory\n");
   }
@@ -810,22 +924,45 @@ struct SharedMyPair {
     a = ap;
     b = bp;
     flag = f;
+    start_time_ = std::chrono::high_resolution_clock::now();
   }
   std::string flag;
+  std::chrono::high_resolution_clock::time_point start_time_;
   double a, b;
+  
   double multiply() { 
     printf("SharedMyPair::multiply | \n");
-    return a*b; }
-  double divide(double c, double d) { 
-    printf("SharedMyPair::divide | \n");
-    return c / d; }
+    return a*b; 
+  }
+  
+  double divideJustWorkInWorkerThread(double c, double d) {
+    printf("SharedMyPair::divideJustWorkInWorkerThread | flag:%s, Is In SmartThreadWorkerFlag Thread:%d\n", flag.c_str(), smartThreadPool->IsInThread(SmartThreadWorkerFlag));
+    return c / d; 
+  }
+  
   double divideDelay(double c, double d) { 
-    printf("SharedMyPair::divideDelay | flag:%s\n", flag.c_str());
-    return c / d; }
+    std::chrono::duration<double, std::milli> ms = std::chrono::high_resolution_clock::now() - start_time_;
+    printf("SharedMyPair::divideDelay | flag:%s, acture time delay:%f, Is In SmartThreadCPUFlag Thread:%d\n", flag.c_str(), ms.count(), smartThreadPool->IsInThread(SmartThreadCPUFlag));
+    return c / d; 
+  }
+
+
+  double divideJustWorkInWorkerThreadWrapper(double c, double d) {
+    double ans = 0;
+    if (smartThreadPool->IsInThread(SmartThreadWorkerFlag)){
+      printf("SharedMyPair::divideJustWorkInWorkerThreadWrapper | flag:%s, Is In SmartThreadWorkerFlag Thread\n", flag.c_str());
+      ans = divideJustWorkInWorkerThread(c,d);
+    }else {
+      printf("SharedMyPair::divideJustWorkInWorkerThreadWrapper | flag:%s, this_threadid:%s, not Is In SmartThreadWorkerFlag Thread, post to SmartThreadWorkerFlag\n", flag.c_str(), smartThreadPool->GetThreadIdOfString(std::this_thread::get_id()).c_str());
+      auto res = smartThreadPool->ApplyAsync(SmartThreadWorkerFlag, TaskPriority::MEDIUM, std::bind(&SharedMyPair::divideJustWorkInWorkerThread, shared_from_this(),c,d));
+      ans = res.get();
+    }
+    return ans;
+  }
 };
 
-void do_smart_thread_pool_cpp11_test(){
 
+int main(int argc, char** argv) {
   // ********************How to init `SmartThreadPool`********************
   //
   // using stp::SmartThreadPool;
@@ -837,22 +974,22 @@ void do_smart_thread_pool_cpp11_test(){
   //                         uint8_t capacity,
   //                         uint8_t init_size);
   // ******** Such as:
-  // builder.AddClassifyPool("DefaultPool", 16, 4)
-  //        .AddClassifyPool("CPUBoundPool", 8, 4)
-  //        .AddClassifyPool("IOBoundPool", 16, 8)
-  // auto pool = builder.BuildAndInit();  // will block current thread
+  // builder.AddClassifyPool(SmartThreadUIFlag, 16, 4)
+  //        .AddClassifyPool(SmartThreadCPUFlag, 8, 4)
+  //        .AddClassifyPool(SmartThreadIOFlag, 16, 8)
+  // auto smartThreadPool = builder.BuildAndInit();  // will block current thread
   //
   // ***********************************************************************
 
   // ******************************How to join a task******************************
   //
-  // pool->ApplyAsync(function, args...);
+  // smartThreadPool->ApplyAsync(function, args...);
   // ******** Such as:
   // 1. Run a return careless task.
-  // pool->ApplyAsync("IOBoundPool", TaskPriority::MEDIUM, [](){ //DoSomeThing(args...); }, arg1, arg2, ...);
+  // smartThreadPool->ApplyAsync(SmartThreadIOFlag, TaskPriority::MEDIUM, [](){ //DoSomeThing(args...); }, arg1, arg2, ...);
   //
   // 2. Run a return careful task.
-  // auto res = pool->ApplyAsync("CPUBoundPool", TaskPriority::HIGH, [](int count){ return count; }, 666);
+  // auto res = smartThreadPool->ApplyAsync(SmartThreadCPUFlag, TaskPriority::HIGH, [](int count){ return count; }, 666);
   // auto value = res.get();
   //
   // or you can set a timeout duration to wait for the result to become available.
@@ -866,25 +1003,25 @@ void do_smart_thread_pool_cpp11_test(){
   //
   // *******************************************************************************
 
-  using stp::SmartThreadPoolBuilder;
-  using stp::TaskPriority;
-  SmartThreadPoolBuilder builder;
-  builder.AddClassifyPool("DefaultPool", 1, 1)
-    .AddClassifyPool("CPUBoundPool", 1, 1)
-    .AddClassifyPool("IOBoundPool", 64, 32);
-    //.EnableMonitor(std::chrono::seconds(5));
-  auto pool = builder.BuildAndInit();
 
-  for (int i = 0; i < 64; ++i) {
+  SmartThreadPoolBuilder builder;
+  builder.AddClassifyPool(SmartThreadCPUFlag, 1, 1)
+    .AddClassifyPool(SmartThreadIOFlag, 64, 32)
+    .AddClassifyPool(SmartThreadUIFlag, 1, 1)
+    .AddClassifyPool(SmartThreadWorkerFlag, 1, 1);
+    //.EnableMonitor(std::chrono::seconds(5));
+  smartThreadPool = builder.BuildAndInit();
+
+  for (int i = 0; i < 2; ++i) {
     for (unsigned char j = 0; j < 5; ++j) {
-      pool->ApplyAsync("IOBoundPool", static_cast<TaskPriority>(j), [](int priority,  unsigned char i) {
+      smartThreadPool->ApplyAsync(SmartThreadIOFlag, static_cast<TaskPriority>(j), [](int priority,  unsigned char i) {
         std::this_thread::sleep_for(std::chrono::seconds(5));
         printf("IOBoundPool priority:%d, %d\n", priority, i);
       },j, i);
     }
   }
 
-  pool->ApplyAsync("IOBoundPool", TaskPriority::HIGH, [](){
+  smartThreadPool->ApplyAsync(SmartThreadIOFlag, TaskPriority::HIGH, [](){
     int repeat_times = 5;
     while (--repeat_times >= 0) {
       printf("IOBoundPool HIGH Task\n"); 
@@ -894,7 +1031,7 @@ void do_smart_thread_pool_cpp11_test(){
 
   {
     //添加lambda函数OK
-    auto res = pool->ApplyAsync("CPUBoundPool", TaskPriority::MEDIUM, [](int x, int y){
+    auto res = smartThreadPool->ApplyAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, [](int x, int y){
       return x + y;
     }, 1, 2);
     printf("added result: %d\n", res.get());
@@ -902,43 +1039,59 @@ void do_smart_thread_pool_cpp11_test(){
 
   {
     //添加使用bind封装的无参函数OK
-    auto res = pool->ApplyAsync("CPUBoundPool", TaskPriority::MEDIUM, std::bind(my_divide, 1, 2));
+    auto res = smartThreadPool->ApplyAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, std::bind(my_divide, 1, 2));
     //添加函数指针函数OK
-    auto res1 = pool->ApplyAsync("CPUBoundPool", TaskPriority::MEDIUM, my_divide, 1, 2);
+    auto res1 = smartThreadPool->ApplyAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, my_divide, 1, 2);
     printf("my_divide result: %f\n", res.get());
   }
 
   {
     MyPair mypair{ 1, 2 };
-    auto res = pool->ApplyAsync("CPUBoundPool", TaskPriority::MEDIUM, std::bind(&MyPair::multiply, mypair));
+    //无shared_ptr保护的变量，要等待，否则在执行时，mypair可能已被删除，从而会有野崩溃
+    auto res = smartThreadPool->ApplyAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, std::bind(&MyPair::multiply, mypair));
     printf("multiply result: %f\n", res.get());
-    auto res1 = pool->ApplyAsync("CPUBoundPool", TaskPriority::MEDIUM, std::bind(&MyPair::divide, mypair, 1, 2));
+    auto res1 = smartThreadPool->ApplyAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, std::bind(&MyPair::divide, mypair, 1, 2));
     printf("divide result: %f\n", res1.get());
   }
 
-  {
-    //添加shared_ptr对象OK
-    std::shared_ptr<SharedMyPair> shard_pair(new SharedMyPair(1, 2, ""));
-    auto res = pool->ApplyAsync("CPUBoundPool", TaskPriority::MEDIUM, std::bind(&SharedMyPair::divide, shard_pair, 1, 2));
-    //添加mem_fn封装的类成员函数OK
-    auto res1 = pool->ApplyAsync("CPUBoundPool", TaskPriority::MEDIUM, std::mem_fn(&SharedMyPair::divide), shard_pair, 1, 2);
-    printf("shard_pair divide result: %f\n", res.get());
-  }
-
-  {
+  { 
+    //添加延迟执行函数OK！！!从日志是看，延迟1000ms执行的任务，可以会在[1000，1005]ms单被执行，即定时任务会被延迟执行
     std::shared_ptr<SharedMyPair> shard_pair1(new SharedMyPair(1, 2, "sleep 10000"));
+    auto res = smartThreadPool->ApplyDelayAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, std::chrono::milliseconds(10000), std::mem_fn(&SharedMyPair::divideDelay), shard_pair1, 1, 2);
     std::shared_ptr<SharedMyPair> shard_pair2(new SharedMyPair(1, 2, "sleep 6000"));
-    std::shared_ptr<SharedMyPair> shard_pair3(new SharedMyPair(1, 2, "sleep 1000"));
-    //添加延迟执行函数OK！！！
-    auto res = pool->ApplyDelayAsync("CPUBoundPool", TaskPriority::MEDIUM, std::chrono::milliseconds(10000), std::mem_fn(&SharedMyPair::divideDelay), shard_pair1, 1, 2);
-    auto res1 = pool->ApplyDelayAsync("CPUBoundPool", TaskPriority::MEDIUM, std::chrono::milliseconds(6000), std::bind(&SharedMyPair::divideDelay, shard_pair2, 1, 2));
+    auto res1 = smartThreadPool->ApplyDelayAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, std::chrono::milliseconds(6000), std::bind(&SharedMyPair::divideDelay, shard_pair2, 1, 2));
     //添加延迟执行bind + placeholders函数OK！！！
-    auto res2 = pool->ApplyDelayAsync("CPUBoundPool", TaskPriority::MEDIUM, std::chrono::milliseconds(1000), std::bind(&SharedMyPair::divideDelay, shard_pair3, 1, std::placeholders::_1), 2);
-    printf("shard_pair divide result: %f\n", res.get());
+    std::shared_ptr<SharedMyPair> shard_pair3(new SharedMyPair(1, 2, "sleep 1000"));
+    auto res2 = smartThreadPool->ApplyDelayAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, std::chrono::milliseconds(1000), std::bind(&SharedMyPair::divideDelay, shard_pair3, 1, std::placeholders::_1), 2);
+    // printf("shard_pair3 divide result: %f\n", res2.get());
+    // printf("shard_pair2 divide result: %f\n", res1.get());
+    // printf("shard_pair1 divide result: %f\n", res.get());
   }
 
-  pool->StartAllWorkers();
+  {
+    //类chrome线程模型，可以实现让函数在特定线程（如UI线程，工作线程）中执行
+    std::shared_ptr<SharedMyPair> shard_pair(new SharedMyPair(1, 2, "call from main thread"));
+    auto ans = shard_pair->divideJustWorkInWorkerThreadWrapper(1,2);
+    printf("main thread call divideJustWorkInWorkerThreadWrapper ans1 : %f\n", ans);
+    std::shared_ptr<SharedMyPair> shard_pair1(new SharedMyPair(1, 2, "call from SmartThreadUIFlag"));
+    auto res = smartThreadPool->ApplyAsync(SmartThreadUIFlag, TaskPriority::MEDIUM, std::bind(&SharedMyPair::divideJustWorkInWorkerThreadWrapper, shard_pair1, 1, std::placeholders::_1), 2);
+    printf("SmartThreadUIFlag thread divideJustWorkInWorkerThreadWrapper ans : %f\n", res.get());
+    std::shared_ptr<SharedMyPair> shard_pair2(new SharedMyPair(1, 2, "call from SmartThreadWorkerFlag"));
+    auto res1 = smartThreadPool->ApplyAsync(SmartThreadWorkerFlag, TaskPriority::MEDIUM, std::bind(&SharedMyPair::divideJustWorkInWorkerThreadWrapper, shard_pair2, 1, std::placeholders::_1), 2);
+    printf("SmartThreadWorkerFlag thread divideJustWorkInWorkerThreadWrapper ans : %f\n", res1.get());
+  }
+
+  // {
+  //   //添加shared_ptr对象OK
+  //   std::shared_ptr<SharedMyPair> shard_pair(new SharedMyPair(1, 2, ""));
+  //   auto res = smartThreadPool->ApplyAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, std::bind(&SharedMyPair::divide, shard_pair, 1, 2));
+  //   //添加mem_fn封装的类成员函数OK
+  //   auto res1 = smartThreadPool->ApplyAsync(SmartThreadCPUFlag, TaskPriority::MEDIUM, std::mem_fn(&SharedMyPair::divide), shard_pair, 1, 2);
+  //   printf("shard_pair divide result: %f\n", res.get());
+  // }
+
+
+  smartThreadPool->StartAllWorkers();
+  //任务池没有任务时不会主动退出，ctrl + c 退出
 }
 */
-
-#endif  // SMART_THREAD_POOL_H_
